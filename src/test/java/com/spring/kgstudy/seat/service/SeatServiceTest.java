c90b012c866de39a7a903d7e133afca1e9e4d738
usersyh <syu4809@naver.com>
2023-07-19 22:44:10 +0900

    222

--- src/test/java/com/spring/kgstudy/seat/service/SeatServiceTest.java
+++ src/test/java/com/spring/kgstudy/seat/service/SeatServiceTest.java
@@ -3,12 +3,18 @@ package com.spring.kgstudy.seat.service;
 import java.util.ArrayList;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
 
 import com.spring.kgstudy.seat.vo.SeatVO;
 
 
-
+@WebAppConfiguration
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"file:src/main/webapp/WEB-INF/spring/appServlet/*.xml","file:src/main/webapp/WEB-INF/spring/root-context.xml"})
 class SeatServiceTest {
 
 	
