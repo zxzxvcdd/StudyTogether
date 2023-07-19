7f4808ccee8d6888f17f4dfd5426022c0b6e4179
SHIN <zxzxvcdd@gmail.com>
2023-07-19 16:35:05 +0900

    junit5

--- src/main/resources/ddl/pass.sql
+++ src/main/resources/ddl/pass.sql
@@ -14,7 +14,7 @@ pass_id number constraint pass_id_pk primary key
 , pass_name varchar2(200)
 , pass_state vatchar2(200)
 , constraint pass_user_fk foreign key (user_id) references meber(user_id)
-, constraint pass_order_fk foreign key (order_id) references order_table(user_id)
+, constraint pass_order_fk foreign key (order_id) references order_table(order_id)
 , constraint pass_type_ck check(pass_type in('time','seson')
 ,
 
