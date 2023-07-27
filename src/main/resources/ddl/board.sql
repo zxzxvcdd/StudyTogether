efdc1e801fdba834272e9eb48fe7bf7b423c700a
juyeon <jsfoxi1227@naver.com>
2023-07-27 09:41:36 +0900

    ddl 수정

--- src/main/resources/ddl/board.sql
+++ src/main/resources/ddl/board.sql
@@ -3,17 +3,19 @@
 CREATE TABLE board (
 board_id NUMBER CONSTRAINT board_id_pk PRIMARY KEY
 , user_id VARCHAR(200)
-, board_date DATE
-, board_title VARCHAR(200)
-, board_content VARCHAR(200)
-, board_writer VARCHAR(200)
-, board_hitup NUMBER
-, board_filename VARCHAR(200)
-, ref NUMBER
-, re_step NUMBER
-, re_level NUMBER
-, CONSTRAINT fk_user_id_board FOREIGN KEY (user_id) REFERENCES member(user_id)
+, title VARCHAR(200)
+, content VARCHAR(200)
+, writer VARCHAR(200)
+, indate DATE
+, count NUMBER
+--, board_filename VARCHAR(200)
+, boardGroup NUMBER
+, boardSequence NUMBER
+, boardLevel NUMBER
+, boardAvailable NUMBER
+--, CONSTRAINT fk_user_id_board FOREIGN KEY (user_id) REFERENCES member(user_id)
 );
+
 
 CREATE SEQUENCE board_seq
 INCREMENT BY 1
