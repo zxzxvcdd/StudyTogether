a5274cd416a3559c64ed48169d1da5cc9ec260b6
SHIN <zxzxvcdd@gmail.com>
2023-07-27 09:41:05 +0900

    board

--- src/main/java/com/spring/kgstudy/seat/restcontroller/SeatRestController.java
+++ src/main/java/com/spring/kgstudy/seat/restcontroller/SeatRestController.java
@@ -1,6 +1,8 @@
 package com.spring.kgstudy.seat.restcontroller;
 
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
